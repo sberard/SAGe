eaf3dfdc64d92bd09a4285f6cad1286c1db0d3d8
cchauve <cchauve@ssck10516-x01.math.sfu.ca>
2018-07-16 09:39:28 -0700

    scores script and results

---
+++ exp/2018_07_15_D10T0L20I10T10_NNI_DECOSTAR/SAGe_scores.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "#dataset rec_score DeCo_score linearity_score" > results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_025/ lambda_025 >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_05/ lambda_05   >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_1/ lambda_1     >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_2/ lambda_2     >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_3/ lambda_3     >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_5/ lambda_5     >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_7/ lambda_7     >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_10/ lambda_10   >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_20/ lambda_20   >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_30/ lambda_30   >> results/summary_1
+python code/DeCoSTAR_scores.py results/DeCoSTAR/lambda_50/ lambda_50   >> results/summary_1
